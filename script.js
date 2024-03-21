140a541131c6dec2191f861ee59b904cf409dadf
Simran-jamal <153733270+Simran-jamal@users.noreply.github.com>
2024-03-21 15:44:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -109,7 +109,7 @@ function playerGame(playerMove) {
     updateScoreElement();
     document.querySelector('.js-result').innerHTML = result;
 
-    document.querySelector('.js-moves').innerHTML = `You ${playerMove}  ${computerMove} Computer`;
+    document.querySelector('.js-moves').innerHTML = `You : ${playerMove} ,  Computer : ${computerMove}`;
 }
 function updateScoreElement() {
     document.querySelector('.js-score')
